54a5b3190307b7f3ed45a172ccc8a783e30488ad
timothee <tim.dorand@gmail.com>
2016-03-30 09:12:25 +0200

    début app_dev

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,11 +3,17 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends Controller
 {
+    /**
+     * Route pour faire n'importe quoi
+     * @Route()
+     */
     public function indexAction()
     {
-        return $this->render('AppBundle:Default:index.html.twig');
+        echo "Hello World !";
+
     }
 }
