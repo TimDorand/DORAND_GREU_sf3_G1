c44d9c425e31dd98c6650c25ad4aa8fbbbd7a93b
timothee <tim.dorand@gmail.com>
2016-03-30 11:07:36 +0200

    Article COntroller

--- src/AppBundle/Controller/Article/ArticleController.php
+++ src/AppBundle/Controller/Article/ArticleController.php
@@ -1,25 +1,28 @@
 <?php
-
 namespace AppBundle\Controller\Article;
-    use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-    use Symfony\Component\HttpFoundation\Response;
-    use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 class ArticleController extends Controller
 {
-    public function listAction()
-    {
-
-    }
     /**
-     * @Route() ("/{id}")
-     * @param id
+     * @Route("/{id}", requirements={"id" = "\d+"})
+     *
+     * @param $id
      *
      * @return Response
      */
-    public function showAction($id)
+    public function showAction($id, Request $request)
     {
-        return new Response('Article avec l\'id'.$id);
+        $tag = $request->query->get('tag');
+        return new Response('Article avec l\'id '.$id.' avec le tag: '.$tag);
     }
-
-
+    /**
+     * @Route("/list")
+     */
+    public function listAction()
+    {
+        return new Response('List of article');
+    }
 }
